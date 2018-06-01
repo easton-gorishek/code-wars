23f7ac82c1286168ff0e7aaba8b2d0d7a5996243
Easton Gorishek <ieaston26@gmail.com>
2018-05-31 22:25:35 -0700

    completed kata

---
+++ insert-dashes/README.md
@@ -0,0 +1,8 @@
+## Code Wars Link
+* https://www.codewars.com/kata/insert-dashes
+
+## Problem Domain
+* Insert dashes between two odd numbers
+
+## Whiteboard
+* Worked on this with Robyn Navarro
--- insert-dashes/solution.js
+++ insert-dashes/solution.js
@@ -1,15 +1,13 @@
 function insertDash(num) {
-  
-  let arr = num.toString().split();
-  for(let i = 0; i < arr.length; i ++) {
-    if(parseInt(arr[i]) % 2 === 1 && parseInt(arr[i + 1]) === 1) {
+  let arr = num.toString().split('');
+  console.log(arr);
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 === 1 && arr[i + 1] % 2 === 1) {
       arr.splice(i + 1, 0, '-');
+      console.log(arr);
     }
-    else {
-      continue;
-    }
-    return arr.join();
   }
+  return arr.join('');
 }
 
-insertDash(12345);
+
